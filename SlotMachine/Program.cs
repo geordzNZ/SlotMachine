38395aeccea17220d977a6de95f4fbc6ad0e9ff1
GeordzNZ <geordie.king@gmail.com>
2023-04-23 14:21:13 +0100

    Create slot grid and save data

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -10,8 +10,23 @@
             Console.Clear();
             Console.WriteLine("\t\t\tWELCOME");
             Console.WriteLine("\t\tSlot Machine Game");
-            Console.WriteLine($"\t\tTBC");
+            Console.WriteLine($"\t\tWager on 1 or multiple lines");
             Console.WriteLine($"{DIVIDER}\n");
+
+            // Generate slot grid
+            int[,] slotGrid = new int[3, 3];
+            
+            for (int i=0; i<3;i++)
+            {
+                for (int j=0; j<3; j++)
+                {
+                    Random randomSlot = new Random();
+                    int randomSlotValue = randomSlot.Next(4);
+                    slotGrid[i, j] = randomSlotValue;
+                    Console.Write($"{randomSlotValue} ");
+                }
+            }
+            Console.WriteLine($"\n\n{DIVIDER}\n\n");
         }
     }
 }
