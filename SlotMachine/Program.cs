32d2690f9f1bbff4b861bc55db5737ee58c89c3c
GeordzNZ <geordie.king@gmail.com>
2023-04-23 13:50:34 +0100

    Add project files.

---
+++ SlotMachine/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SlotMachine
+{
+    internal class Program
+    {
+        const string DIVIDER = "================================================================";
+        const string BLANKER = "                                                                ";
+        static void Main(string[] args)
+        {
+            // Header section
+            Console.Clear();
+            Console.WriteLine("\t\t\tWELCOME");
+            Console.WriteLine("\t\tSlot Machine Game");
+            Console.WriteLine($"\t\tTBC");
+            Console.WriteLine($"{DIVIDER}\n");
+        }
+    }
+}
