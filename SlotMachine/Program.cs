04c0c11935951f53ea3087ce1b0f2f21354a738e
GeordzNZ <geordie.king@gmail.com>
2023-05-02 14:22:50 +0100

    #4 first try at full game logic, using subset of line options

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -8,28 +8,21 @@ namespace SlotMachine
         const int START_MONEY_VALUE = 5;
         const int GRID_ROWS = 3;
         const int GRID_COLUMNS = 3;
-        const string DIVIDER = "================================================================";
-        const string BLANKER = "                                                                ";
+        const string DIVIDER = "==========================================================================";
+        const string BLANKER = "                                                                          ";
 
         static void Main(string[] args)
         {
             // Game set up, defaulted for now, but could be made user input
-            int linesPlayed = 1;
-            int lineStake = 1;
+            int linesPlayed = 0;
+            //int lineStake = 1;
             int moniesAmount = START_MONEY_VALUE;
 
             char playAgainAnswer = ' ';
             do
             {
-                // Output Header section
-                Console.Clear();
-                Console.WriteLine("\t\t\tWELCOME");
-                Console.WriteLine("\t\t   Slot Machine Game");
-                Console.WriteLine($"\t    Wager on the middle horizontal row - 1 money per line");
-                Console.WriteLine($"\t    You currrently have {moniesAmount} monies");
-                Console.WriteLine($"{DIVIDER}\n");
-
-                // Generate slot grid data
+                // Set up for start of game
+                int winCounter = 0;
                 int[,] slotGrid = new int[GRID_ROWS, GRID_COLUMNS];
                 Random randomSlot = new Random();
 
@@ -39,11 +32,67 @@ namespace SlotMachine
                     {
                         int randomSlotValue = randomSlot.Next(SLOTS_MAX_VALUE);
                         slotGrid[generateRowCounter, generateColumnCounter] = randomSlotValue;
-                        //Console.Write($"{randomSlotValue} ");
                     }
                 }
 
+                // Output Header section
+                Console.Clear();
+                Console.WriteLine("\t\t\tWELCOME");
+                Console.WriteLine("\t\t   Slot Machine Game");
+                Console.WriteLine($"\tWager on matching lines - 1 monies(m) per line");
+
+                // Option A -- pros, less user input / cons, lots of options
+                Console.WriteLine($"\tGame Control Menu:");
+                Console.WriteLine($"\t\tRows:\t\ta = Middle row (1m) / b = All rows (3m)");
+                Console.WriteLine($"\t\tColumns:\tc = All columns (3m)");
+                Console.WriteLine($"\t\tDiagonals:\td = Both Diagonals (2m)");
+                Console.WriteLine($"{DIVIDER}");
+
+                Console.WriteLine($"\t    You currrently have {moniesAmount} monies");
+                Console.WriteLine($"{DIVIDER}\n");
+
+                // Game option input and check amount wagered
+                char gameOptionAnswer = ' ';
+                do
+                {
+                    Console.WriteLine($"\n\tChoose lines based on game menu above.");
+                    Console.Write($"\tOptions (a/b/c/d): ");
+                    ConsoleKeyInfo gameOptionInput = Console.ReadKey();
+
+                    if ((int)gameOptionInput.KeyChar >= 97 && (int)gameOptionInput.KeyChar <= 100) // a / b / c / d
+                    {
+                        gameOptionAnswer = char.Parse(gameOptionInput.KeyChar.ToString());
+
+                        // Assign lines played
+                        switch (gameOptionAnswer)
+                        {
+                            case 'a':
+                                linesPlayed = 1;
+                                break;
+                            case 'b':
+                                linesPlayed = 3;
+                                break;
+                            case 'c':
+                                linesPlayed = 3;
+                                break;
+                            case 'd':
+                                linesPlayed = 2;
+                                break;
+                        }
+
+                        if (moniesAmount < linesPlayed)
+                        {
+                            Console.WriteLine($"\n\t\tNot enough monies ... choose again!!");
+                            continue;
+                        }
+                        break;
+                    }
+                } while (true);  // Game option input loop
+
+ 
+
                 // Output slot grid on screen
+                Console.WriteLine($"\n\n\t\tSlot Grid");
                 for (int outputRowCounter = 0; outputRowCounter < GRID_ROWS; outputRowCounter++)
                 {
                     string rowDetail = "";
@@ -52,35 +101,93 @@ namespace SlotMachine
                         rowDetail += slotGrid[outputRowCounter, outputColumnCounter];
                     }
                     char[] rowDetailChars = rowDetail.ToArray();
-                    //Console.WriteLine($"Row {k} is {String.Join(' ', rowDetailChars)}");
-                    Console.WriteLine($"\t\t{String.Join(' ', rowDetailChars)}");
+                    Console.Write($"\t\t  {String.Join(' ', rowDetailChars)}\n");
                 }
 
-                // Check middle row for matches
+                // Check for matches in appropriate rows
                 bool win = false;
-                int winLossAmount = lineStake * linesPlayed;
 
-                if (slotGrid[1, 0] == slotGrid[1, 1] && slotGrid[1, 0] == slotGrid[1, 2])
+                // Row Checking section
+                if (gameOptionAnswer == 'a' || gameOptionAnswer == 'b')
                 {
-                    win = true;
-                    moniesAmount += winLossAmount;
+                    // Middle row check - option a / b
+                    if (slotGrid[1, 0] == slotGrid[1, 1] && slotGrid[1, 0] == slotGrid[1, 2])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
+
+                    // Other row check - option b
+                    if (gameOptionAnswer == 'b' )
+                    {
+                        // Top row
+                        if (slotGrid[0, 0] == slotGrid[0, 1] && slotGrid[0, 0] == slotGrid[0, 2])
+                        {
+                            win = true;
+                            winCounter++;
+                        }
+
+                        // Bottom row
+                        if (slotGrid[2, 0] == slotGrid[2, 1] && slotGrid[2, 0] == slotGrid[2, 2])
+                        {
+                            win = true;
+                            winCounter++;
+                        }
+                    }
                 }
-                else
+
+                // Column Checking section
+                if (gameOptionAnswer == 'c' )
                 {
-                    moniesAmount -= winLossAmount;
+                    // Left column check - option c
+                    if (slotGrid[0, 0] == slotGrid[1, 0] && slotGrid[0, 0] == slotGrid[2, 0])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
+                    // Centre column check - option c
+                    if (slotGrid[0, 1] == slotGrid[1, 1] && slotGrid[0, 1] == slotGrid[2, 1])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
+                    // Right column check - option c
+                    if (slotGrid[0, 2] == slotGrid[1, 2] && slotGrid[0, 2] == slotGrid[2, 2])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
+                }
+
+                // Diagonals Checking section
+                if (gameOptionAnswer == 'd' )
+                {
+                    // Top left to bottom right check - option d
+                    if (slotGrid[0, 0] == slotGrid[1, 1] && slotGrid[0, 0] == slotGrid[2, 2])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
+                    // Top right to bottom left check - option d
+                    if (slotGrid[0, 2] == slotGrid[1, 1] && slotGrid[0, 2] == slotGrid[2, 0])
+                    {
+                        win = true;
+                        winCounter++;
+                    }
                 }
 
                 // Output win / loss outcome and sign off
                 if (win)
                 {
-                    Console.WriteLine($"\n\tMIDDLE ROW MATCH - CONGRATULATIONS!!!");
+                    Console.WriteLine($"\n\tWINNER WINNER - CONGRATULATIONS!!!");
+                    Console.WriteLine($"\t{winCounter} MATCHED!!!");
                 }
                 else
                 {
-                    Console.WriteLine($"\n\tNO MATCHES THIS ROUND!!!");
+                    Console.WriteLine($"\n\tNO MATCHES THIS SPIN!!!");
                 }
 
-                Console.WriteLine($"\tYou now have {moniesAmount} monies");
+                Console.WriteLine($"\tYou now have {moniesAmount += (-linesPlayed + winCounter)} monies");
                 Console.WriteLine($"{DIVIDER}\n");
 
                 // Play again loop
@@ -89,7 +196,7 @@ namespace SlotMachine
                     Console.Write($"\n\tDo you what to play again? (y/n): ");
                     ConsoleKeyInfo playAgainInput = Console.ReadKey();
 
-                    if ((int)playAgainInput.KeyChar == 110 || (int)playAgainInput.KeyChar == 121) // n or y
+                    if ((int)playAgainInput.KeyChar == 110 || (int)playAgainInput.KeyChar == 121) // only allow - n or y
                     {
                         playAgainAnswer = char.Parse(playAgainInput.KeyChar.ToString());
                         break;
